88ce4ceac1e412abbcf382cb0a23d11359891f18
Dune Desormeaux <dune.desormeaux@gmail.com>
2024-04-29 19:30:33 -0700

    sample and hold fixed

--- fifths.lua
+++ fifths.lua
@@ -11,6 +11,12 @@ thirteen_windows = {-4.97,-4.5,-3.5,-2.5,-1.5,-0.5,0.5,1.5,2.5,3.5,4.5,4.97}
 -- always choosing an octave with a note as close as possible to the key center (0)
 circle_of_fifths = {-6,1,-4,3,-2,5,0,-5,2,-3,4,-1,6}
 
+-- half-way between a minor third (3/12) and a major third (4/12) in 1V per Octave terms
+ambiguous_third = 7/24
+
+-- enable last value store in case line 61 below is uncommented
+last_value = input[1]
+
 --build a major scale for any root note (tonic)
 function major_scale(tonic)
     local scale = {}
@@ -28,31 +34,45 @@ function major_scale(tonic)
     return scale
 end
 
--- choose output values based on input 1 and offsets based on fifths. 0V is assumed to be tuned to C3 (not mandatory!)
+new_key = major_scale(0)
+
+-- choose output values based on input 1 and offsets. offsets are "ambiguous" thirds, meaning they are 
+-- half way between a major and minor third, allowing the tuning and key changes to have maximum effect
 input[1].scale = function(x) 
-    local fifth = 7 / 12
-    local relative_to_c3 = x.volts + 2
-    output[1].volts = relative_to_c3
-    output[2].volts = relative_to_c3 + fifth
-    output[3].volts = relative_to_c3 + fifth + fifth
+
+    --last value is only used if line 61 is uncommented to allow retuning upon input 2 changes
+    last_value = x
+
+    --update output tuning to new key (behaves like sample and hold of input 2 value)
+    for i=1,3 do
+        output[i].scale(new_key)
+    end
+
+    output[1].volts = x.volts
+    output[2].volts = x.volts + ambiguous_third
+    output[3].volts = x.volts + ambiguous_third + ambiguous_third
     output[4]() -- pulse output 4 when tuning
 end
 
 -- when input 2 hops between windows, choose a new key from the circle of fifths
 input[2].window = function(x) 
-    local new_key = major_scale(circle_of_fifths[x])
-    input[1].mode('scale', new_key)
+    new_key = major_scale(circle_of_fifths[x])
+    -- Uncomment the line below to add retriggering/tuning upon input 2 window changes
+    -- input[1].scale(last_value)
 end
 
 --initialize things
 function init()
+
+    --input events fire whenever the input signal moves between semi-tones
+    input[1].mode('scale', {})
 
     --input 2 produces value 1 at -5V and value 12 at +5V
     input[2].mode('window',thirteen_windows,0.2)
 
     --quantize all tuned outputs to the chromatic scale in case our fifths get weird
     for i=1,3 do
-        output[i].scale({})
+        output[i].scale(new_key)
     end
 
     --start in C major (ie. 0V at input 2)
