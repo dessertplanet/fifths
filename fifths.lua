92946849662cb5e3d538739f5193ad777a3f4f8b
Dune Desormeaux <dune.desormeaux@gmail.com>
2024-08-05 15:38:13 -0700

    replace pulses with harmonic series scale

--- fifths.lua
+++ fifths.lua
@@ -11,12 +11,6 @@ thirteen_windows = {-4.97,-4.5,-3.5,-2.5,-1.5,-0.5,0.5,1.5,2.5,3.5,4.5,4.97}
 -- always choosing an octave with a note as close as possible to the key center (0)
 circle_of_fifths = {-6,1,-4,3,-2,5,0,-5,2,-3,4,-1,6}
 
--- half-way between a minor third (3/12) and a major third (4/12) in 1V per Octave terms
-ambiguous_third = 7/24
-
--- enable last value store in case line XX below is uncommented
-last_value = input[1]
-
 --build a major scale for any root note (tonic)
 function major_scale(tonic)
     local scale = {}
@@ -53,7 +47,6 @@ end
 
 function just_fifths_scale(tonic)
     local scale = {}
-    local just_fifth = 1.5
     scale = {
         tonic,
         tonic + just12(3/2)
@@ -61,9 +54,23 @@ function just_fifths_scale(tonic)
     return scale
 end
 
+function harmonic_series(tonic)
+    local scale = {}
+    local series = {1,9/8,10/8,11/8,12/8,13/8,14/8}
+
+    scale = just12(series)
+
+   for i in ipairs(series) do
+        scale[i] = just12(series[i]) + tonic
+   end
+
+    return scale
+end
+
 new_major_key = major_scale(0)
 new_melodic_key = lydian_dominant_scale(0)
 new_fifths_key = just_fifths_scale(0)
+new_harmonic_series = harmonic_series(0)
 
 -- choose output values based on input 1 and offsets. offsets are "ambiguous" thirds, meaning they are 
 -- half way between a major and minor third, allowing the tuning and key changes to have maximum effect
@@ -72,19 +79,15 @@ input[1].scale = function(x)
     --last value is only used if line XX is uncommented to allow retuning upon input 2 changes
     last_value = x
 
-    --update output tuning to new key (behaves like sample and hold of input 2 value)
-    -- for i=1,3 do
-    --     output[i].scale(new_key)
-    -- end
-
     output[1].scale(new_fifths_key)
     output[2].scale(new_major_key)
     output[3].scale(new_melodic_key)
+    output[4].scale(new_harmonic_series)
 
     output[1].volts = x.volts
     output[2].volts = x.volts
     output[3].volts = x.volts
-    output[4]() -- pulse output 4 when tuning
+    output[4].volts = x.volts
 end
 
 -- when input 2 hops between windows, choose a new key from the circle of fifths
@@ -92,8 +95,7 @@ input[2].window = function(x)
     new_major_key = major_scale(circle_of_fifths[x])
     new_melodic_key = lydian_dominant_scale(circle_of_fifths[x])
     new_fifths_key = just_fifths_scale(circle_of_fifths[x])
-    -- Uncomment the line below to add retriggering/tuning upon input 2 window changes
-    -- input[1].scale(last_value)
+    new_harmonic_series = harmonic_series(circle_of_fifths[x])
 end
 
 --initialize things
@@ -105,14 +107,7 @@ function init()
     --input 2 produces value 1 at -5V and value 12 at +5V
     input[2].mode('window',thirteen_windows,0.2)
 
-    --quantize all tuned outputs to the chromatic scale in case our fifths get weird
-    for i=1,3 do
-        output[i].scale(new_key)
-    end
 
     --start in C major (ie. 0V at input 2)
     input[2].window(7)
-
-    -- set up output 4 pulses
-    output[4].action = pulse(0.01, 5)
 end
